24f6130b53e00d76c32a3b3c13ae7384275b38c0
snide02 <michaelsnider2002@gmail.com>
2023-12-01 23:42:27 -0500

    Display potValue
    Potentiometer value is display on the serial monitor.  Link fixed to take you to the actual article.
    
    To use ESC, it is only necessary to connect the signal wire and ground wire.  The +5 volts wire can be used to power the Arduino, if desired.

--- esc/esc.ino
+++ esc/esc.ino
@@ -1,6 +1,6 @@
 /*
         Arduino Brushless Motor Control
-     by Dejan, https://howtomechatronics.com
+     by Dejan, https://howtomechatronics.com/tutorials/arduino/arduino-brushless-motor-control-tutorial-esc-bldc/
      
      Taken from here so we can test.
 */
@@ -14,10 +14,15 @@ int potValue;  // value from the analog pin
 void setup() {
   // Attach the ESC on pin 9
   ESC.attach(9,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
+  Serial.begin(9600);
 }
 
 void loop() {
   potValue = analogRead(A0);   // reads the value of the potentiometer (value between 0 and 1023)
   potValue = map(potValue, 0, 1023, 0, 180);   // scale it to use it with the servo library (value between 0 and 180)
   ESC.write(potValue);    // Send the signal to the ESC
+  
+  Serial.print("potValue: ");
+  Serial.println(potValue);
+
 }
