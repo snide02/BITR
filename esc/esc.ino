3011d5a85da370a0a4cd13ae6bbe6b2e2ac73200
snide02 <michaelsnider2002@gmail.com>
2023-12-04 02:16:22 -0500

    Test ESC and added more Examples
    ESC can not have the motor spin backwards using a negative value.
    DisplayPortValue.ino combines the esc.ino, AP_SimpleWebServer.ino, and WiFiWebServer.ino.  It turns the Ardunio into an access point and then displays the values of the potentiometer, updating it every 5 seconds.

--- esc/esc.ino
+++ esc/esc.ino
@@ -19,7 +19,7 @@ void setup() {
 
 void loop() {
   potValue = analogRead(A0);   // reads the value of the potentiometer (value between 0 and 1023)
-  potValue = map(potValue, 0, 1023, 0, 180);   // scale it to use it with the servo library (value between 0 and 180)
+  potValue = map(potValue, 0, 1023, -90, 90);   // scale it to use it with the servo library (value between 0 and 180)
   ESC.write(potValue);    // Send the signal to the ESC
   
   Serial.print("potValue: ");
